d81217a3b93ff6b72b811a8281e623733a2cde9b
jekil <alessandro@tanasi.it>
2016-10-26 00:30:32 +0200

    Removed trailing slash

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # Publish settings.
-SITEURL = "https://jekil.github.io/jekil.sexy/"
+SITEURL = "https://jekil.github.io/jekil.sexy"
 
 # Third party services.
 ADD_THIS_ID = 'ra-5121ee9b233b392d'
